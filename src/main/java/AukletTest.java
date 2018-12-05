11ec1a3eb3fd54f57a2fdd53a8a1212687c8680d
Joshua Hufford <joshuamh1135@gmail.com>
2018-12-05 14:51:21 -0500

    Add test and pom files

--- src/main/java/AukletTest.java
+++ src/main/java/AukletTest.java
@@ -1,8 +1,19 @@
 import io.auklet.agent.Auklet;
 
 class AukletTest {
-    public static void main(String[] args) {
+    public static void main(String []arg) {
         Auklet.init("", "");
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+            if(i == 990) {
+                try {
+                    System.out.print(i / 0);
+                } catch (ArithmeticException e) {
+                    Auklet.exception(e);
+                    System.out.println("Divide By Zero Error");
+                    System.out.println(e);
+                }
+            }
+        }
     }
 }
