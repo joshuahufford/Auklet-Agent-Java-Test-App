de6fb4017a7059490086ec4621bad6cf2fefef6c
Joshua Hufford <joshuamh1135@gmail.com>
2018-12-05 13:02:13 -0500

    Add initial files

---
+++ src/main/java/AukletTest.java
@@ -0,0 +1,8 @@
+import io.auklet.agent.Auklet;
+
+class AukletTest {
+    public static void main(String[] args) {
+        Auklet.init("", "");
+        System.out.println("Hello, World!");
+    }
+}
